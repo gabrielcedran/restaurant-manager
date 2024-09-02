26f92f6e8503e46390edc5e275a46efb7a533934
gabrielcedran <gabrielcedran@gmail.com>
2024-09-02 22:09:44 +0100

    Order metrics dashboard

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,23 @@
+import { Helmet } from 'react-helmet-async'
+
+import { MonthCancelledOrdersCard } from './month-cancelled-orders-card'
+import { MonthOrdersCard } from './month-orders-card'
+import { MonthRevenueCard } from './month-revenue-card'
+import { TodayOrdersCard } from './today-orders-card'
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+        <div className="grid grid-cols-4 gap-4">
+          <MonthRevenueCard />
+          <MonthOrdersCard />
+          <TodayOrdersCard />
+          <MonthCancelledOrdersCard />
+        </div>
+      </div>
+    </>
+  )
+}
