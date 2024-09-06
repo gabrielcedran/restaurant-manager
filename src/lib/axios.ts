f1f1173ee6b2d80c882fcae3c086beeae3fdd487
gabrielcedran <gabrielcedran@gmail.com>
2024-09-06 18:46:21 +0100

    Enable authorization (sending all cookies automatically to the be)

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,5 @@ import { env } from '@/env'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true, // used for authz purposes - sends all the cookies to the BE automatically
 })
