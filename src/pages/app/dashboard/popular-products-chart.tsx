a258a9162836393414e711293cd55ba211b9dea8
gabrielcedran <gabrielcedran@gmail.com>
2024-09-10 08:07:10 +0100

    Add loader to popular products and daily turnover metrics

--- src/pages/app/dashboard/popular-products-chart.tsx
+++ src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,14 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
-import { BarChart } from 'lucide-react'
-import {
-  CartesianGrid,
-  Cell,
-  Pie,
-  PieChart,
-  ResponsiveContainer,
-  XAxis,
-  YAxis,
-} from 'recharts'
+import { BarChart, Loader2 } from 'lucide-react'
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
 import colors from 'tailwindcss/colors'
 
 import { getPopularProductsMetrics } from '@/api/get-popular-products'
@@ -47,7 +39,7 @@ export function PopularProductsChart() {
         </div>
       </CardHeader>
       <CardContent>
-        {popularProductsMetrics && (
+        {popularProductsMetrics ? (
           <ResponsiveContainer width="100%" height={240}>
             <PieChart style={{ fontSize: 12 }}>
               <Pie
@@ -102,6 +94,10 @@ export function PopularProductsChart() {
               </Pie>
             </PieChart>
           </ResponsiveContainer>
+        ) : (
+          <div className="flex h-[240px] w-full items-center justify-center">
+            <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
+          </div>
         )}
       </CardContent>
     </Card>
